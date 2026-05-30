bfe028373cb4719ba548a8a3ae08950d49058c39
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-30 13:27:12 +0200

    src/app.js: configure cors to accept localhost name and addresses
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({
+    "origin": [
+        /^https?:\/\/(localhost|127\.0\.0\.1|\[::1\])(:\d+)?$/
+    ],
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200
+}));
 app.use(express.json())
 
 app.get('/', (req, res) => {
