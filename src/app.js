40fcaea0c9df7daeb665a49507f606a47310ef34
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-30 13:28:51 +0200

    src/app.js: fix formatting
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    "origin": [
-        /^https?:\/\/(localhost|127\.0\.0\.1|\[::1\])(:\d+)?$/
-    ],
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 200
+  "origin": [
+    /^https?:\/\/(localhost|127\.0\.0\.1|\[::1\])(:\d+)?$/
+  ],
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 200
 }));
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ ok: true, msg: 'API Movie App funcionando' })
-})
+  res.json({ ok: true, msg: 'API Movie App funcionando' });
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`)
-})
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
