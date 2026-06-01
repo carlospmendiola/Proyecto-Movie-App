73d1944ff097e7b4df4f68b1d5f8df64f88fc7a5
David Santamaría Rogado <howl.nsp@gmail.com>
2026-06-01 12:49:05 +0200

    implement login
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,36 @@
 import { User } from "../models/user.model.js";
+//import { generarToken } from "../utils/gestionarToken.js";
 
-export const login = (req, res) => {
-  return res.status(200).json({
-    ok: true,
-    msg: "logueando"
-  });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const usuario = await User.findOne({ email }).select("password rol");
+    if (!usuario)
+      return res.status(404).json({
+        ok: false,
+        msg: "Usuario inexistente"
+      });
+
+    if (!await usuario.comparePassword(password))
+      return res.status(401).json({
+        ok: false,
+        msg: "Contraseña incorrecta"
+      });
+
+    //const token = generarToken({ id: usuario._id, rol: usuario.rol });
+
+    res.status(200).json({
+      ok: true,
+      //token
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error interno del servidor"
+    });
+  }
 };
 
 export const signup = (req, res) => {
