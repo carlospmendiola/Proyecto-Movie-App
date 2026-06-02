07df197d4ed813a70782819193d175efc0953cc4
David Santamaría Rogado <howl.nsp@gmail.com>
2026-06-02 12:21:56 +0200

    implement buscarPeliculasporTitulo
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -1,4 +1,5 @@
-import { User } from "../models/user.model.js";
+import { matchedData } from "express-validator";
+import { Movie } from "../models/movie.model.js";
 import { findMoviebyIdController } from "../utils/findMovieByIdController.js";
 
 export const buscarPeliculasporID = (req, res) => {
@@ -10,13 +11,31 @@ export const buscarPeliculasporID = (req, res) => {
   });
 };
 
-export const buscarPeliculasporTitulo = (req, res) => {
-  console.log("nuevo token: ", req.token)
-  return res.status(200).json({
-    ok: true,
-    msg: "obteniendo una pelicula por titulo",
-    token: req.token
-  });
+export const buscarPeliculasporTitulo = async (req, res) => {
+  try {
+    const { title } = matchedData(req);
+    // Al usar una variable en una regex hay que generar un nuevo objeto de RegExp, no se puede directamente.
+    const movies = await Movie.find({ title: new RegExp(title, "i") });
+
+    if (!movies?.length)
+      return res.status(404).json({
+        ok: false,
+        msg: "No se encontraron películas",
+        token: req.token
+      });
+
+    return res.status(200).json({
+      ok: true,
+      msg: movies,
+      token: req.token
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error interno del servidor"
+    });
+  }
 };
 
 export const obtenerFavoritos = (req, res) => {
