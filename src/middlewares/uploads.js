31803d86abc35198a55e34b561455299b1cff630
Rafa Alvargonzalez <isildur4@hotmail.com>
2026-06-03 10:05:25 +0200

    Ajuste en la funcionalidad de subir imagenes

--- src/middlewares/uploads.js
+++ src/middlewares/uploads.js
@@ -1,20 +1,17 @@
 import multer from 'multer'
-import { v2 as cloudinary } from 'cloudinary'
-import CloudinaryStoragePkg from 'multer-storage-cloudinary'
+import cloudinary from 'cloudinary'
+import { CloudinaryStorage } from 'multer - storage - cloudinary'
 
-const CloudinaryStorage =
-  CloudinaryStoragePkg.CloudinaryStorage || CloudinaryStoragePkg
-
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary.v2,
   params: {
-    folder: 'movie-app',
+    folder: 'movie- app',
     allowed_formats: ['jpg', 'jpeg', 'png', 'gif']
   }
 })
