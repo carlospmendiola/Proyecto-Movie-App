474dc35784d3b9ec207cc3f4d900e470693c6184
David Santamaría Rogado <howl.nsp@gmail.com>
2026-06-03 21:41:25 +0200

    set more field as required in movie model
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -3,8 +3,8 @@ import { Schema, model } from "mongoose";
 const movieSchema = new Schema({
   title: {
     type: String,
-    trim: true,
     required: true,
+    trim: true,
     maxLength: 255
   },
   image: {
@@ -13,25 +13,30 @@ const movieSchema = new Schema({
   },
   synopsis: {
     type: String,
+    required: true,
     trim: true,
     maxLength: 2000
   },
   year: {
     type: Number,
+    required: true,
     min: 1888, // La primera película de la historia fue en 1888, Roundhay Garden Scene
     max: 9999
   },
   director: {
     type: String,
+    required: true,
     trim: true,
     maxlength: 100
   },
   genres: [{
     type: String,
+    required: true,
     trim: true
   }],
   duration: {
     type: Number,
+    required: true,
     min: 1,
     max: 1000,
     validate: {
