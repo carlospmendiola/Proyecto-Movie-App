b609848c4a89bb6a14298e19ab4f25ebc257412e
David Santamaría Rogado <howl.nsp@gmail.com>
2026-06-03 17:20:11 +0200

    improve login and signup functions
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,22 +4,30 @@ import { generarToken } from "../utils/gestionarToken.js";
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const usuario = await User.findOne({ email }).select("password rol");
-    if (!usuario)
+    const user = await User.findOne({ email }).select("-_id name email rol +password");
+
+    if (!user)
       return res.status(404).json({
         ok: false,
         msg: "Usuario inexistente"
       });
-    if (!await usuario.comparePassword(password))
+
+    if (!await user.comparePassword(password))
       return res.status(401).json({
         ok: false,
         msg: "Contraseña incorrecta"
       });
-      
-    const token = await generarToken({ id: usuario._id, rol: usuario.rol });
+
+    const token = await generarToken({ id: user._id, rol: user.rol });
 
     res.status(200).json({
       ok: true,
+      msg: "Sesión iniciada",
+      user: {
+        user: user.name,
+        email: user.email,
+        rol: user.rol
+      },
       token
     });
   } catch (error) {
@@ -34,6 +42,7 @@ export const login = async (req, res) => {
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
     if (await User.exists({ email }))
       return res.status(409).json({
         ok: false,
@@ -46,6 +55,12 @@ export const signup = async (req, res) => {
 
     res.status(201).json({
       ok: true,
+      msg: "Nuevo usuario registrado",
+      user: {
+        name,
+        email,
+        rol
+      },
       token
     });
   } catch (error) {
